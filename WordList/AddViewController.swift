c0f764af01d88bf54bbe85575149e5f8dc5e2311
harutiro <harutiro2027@outlook.jp>
2021-12-27 15:51:26 +0900

    add: 追加部分を作成

--- WordList/AddViewController.swift
+++ WordList/AddViewController.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class AddViewController: UIViewController {
+    
+    @IBOutlet var englishTextField:UITextField!
+    @IBOutlet var japaneseTextField:UITextField!
+    
+    var wordArray:[Dictionary<String,String>] = []
+    
+    let saveData = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if saveData.array(forKey: "WORD") != nil{
+            wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String,String>]
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveWord(){
+        let wordDctionary =
+        ["english":englishTextField.text! , "japanese":japaneseTextField.text!]
+        
+        wordArray.append(wordDctionary)
+        saveData.set(wordArray ,forKey: "WORD")
+        
+        let alert = UIAlertController(
+            title:"保存完了",
+            message: "単語の登録が完了しました",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(
+            title:"OK",
+            style: .default,
+            handler:nil
+        ))
+        present(alert,animated:true,completion: nil)
+        
+        englishTextField.text = ""
+        japaneseTextField.text = ""
     }
-    */
+    
 
 }
