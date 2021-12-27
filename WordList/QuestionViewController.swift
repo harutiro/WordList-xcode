ab0c10bf53bdc4c818f046a69364418dd072c8a6
harutiro <harutiro2027@outlook.jp>
2021-12-27 15:39:36 +0900

    add:　問題表示部分作成

--- WordList/QuestionViewController.swift
+++ WordList/QuestionViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    
+    var isAnswered: Bool = false
+    var wordArray:[Dictionary<String,String>] = []
+    var nowNumber:Int = 0
+    let saveData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        answerLabel.text = ""
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated:Bool){
+        super.viewWillAppear(animated)
+        wordArray = saveData.array(forKey:"WORD") as! [Dictionary<String,String>]
+        
+        wordArray.shuffle()
+        questionLabel.text = wordArray[nowNumber]["english"]
     }
-    */
+    
+    @IBAction func nextButtonTapped(){
+        if isAnswered{
+//            次の問題へ
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            if nowNumber < wordArray.count{
+//                次の問題を表示
+                questionLabel.text = wordArray[nowNumber]["english"]
+                isAnswered = false
+                nextButton.setTitle("答えを表示", for: .normal)
+            }else{
+                nowNumber = 0
+                performSegue(withIdentifier: "toFinishView", sender: nil)
+            }
+        }else{
+//            答えを表示
+            answerLabel.text = wordArray[nowNumber]["japanese"]
+            isAnswered = true
+            nextButton.setTitle("次へ", for: .normal)
+        }
+    }
+    
+    
 
 }
