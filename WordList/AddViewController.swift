17177925b6dac12a616ead58f720c6dc6561c286
harutiro <harutiro2027@outlook.jp>
2021-12-29 10:12:29 +0900

    add: AddViewControler　確認チェック部分作成

--- WordList/AddViewController.swift
+++ WordList/AddViewController.swift
@@ -18,6 +18,8 @@ class AddViewController: UIViewController {
     
     var editNum:Int! = -1
     
+    var status:Bool = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,8 +41,44 @@ class AddViewController: UIViewController {
         let wordDctionary =
         ["english":englishTextField.text! , "japanese":japaneseTextField.text!]
         
-        if englishTextField.text == ""{return}
-        if japaneseTextField.text == ""{return}
+        
+        //        空白対策
+        if englishTextField.text == "" || japaneseTextField.text == ""{
+            
+            let alert = UIAlertController(
+                title:"入力されていません",
+                message: "入力されていない場所があります",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(
+                title:"OK",
+                style: .default,
+                handler:nil
+            ))
+            present(alert,animated:true,completion: nil)
+            
+            return
+        }
+        
+        //        Word２Vec対策
+        answerOutput()
+        print (status)
+        if !status {
+            
+            let alert = UIAlertController(
+                title:"Word2Vecに対応してません",
+                message: "対応していない単語です、別の単語に変更してください。",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(
+                title:"OK",
+                style: .default,
+                handler:nil
+            ))
+            present(alert,animated:true,completion: nil)
+            
+            return
+        }
         
         if editNum != -1{
             wordArray.remove(at: editNum)
@@ -70,6 +108,8 @@ class AddViewController: UIViewController {
         
         if editNum != -1 {
             editNum = -1
+            
+            //            TODO:編集時は前の画面にもどるようにする
         }
     }
     
@@ -78,6 +118,43 @@ class AddViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-    
-    
+    func answerOutput(){
+        
+        let str:String = japaneseTextField.text!
+        
+        print(str.urlEncoded)
+        
+        struct RecebedMain: Codable{
+            let status:String
+            let mode:String
+            let data:[String]
+        }
+        
+        let jsonUrlString = String("http://127.0.0.1:5000/near?str=\(str.urlEncoded)&get_number=50")
+        
+        guard let url = URL(string: jsonUrlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {return}
+            do{
+                let json = String(data: data, encoding: .utf8)!
+                print(json)
+                
+                let myblog = try JSONDecoder().decode(RecebedMain.self, from: data)
+                print(myblog.status)
+                print(myblog.mode)
+                print(myblog.data)
+                
+                self.status = myblog.status == "OK"
+                
+                
+            } catch let jsonError{
+                print("error", jsonError)
+            }
+        }.resume()
+        
+        
+        
+    }
 }
+
